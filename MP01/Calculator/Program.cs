dbf73a44293a46c5c497b79f7b5cb748dbf56b38
danielasamardgieva <samardzhievad@gmail.com>
2015-10-03 10:05:21 +0300

    Модул6 Видео 3 Стартиране на dll файл от главната програма - архитектура на програмата

---
+++ MP01/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+		//Дефиниране на библиотеки
+		public UserInput.UInput uInput=new UserInput.UInput();
+
+		public static void Main (string[] args)
+		{
+			MainClass _program=new MainClass();
+
+			_program.uInput.justTesting ();
+		}
+	}
+}
---
+++ MP01/UserInput/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserInput
+{
+	public class UInput
+	{
+		public UInput ()
+		{
+		}
+
+		public void justTesting ()
+		{
+			Console.WriteLine("Стартиране на UserIput.UInput.justTesting ()");
+		}
+
+	}
+}
+
