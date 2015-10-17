acd3aad1ccf47c741307d32455eff4263695e335
danielasamardgieva <samardzhievad@gmail.com>
2015-10-17 11:06:40 +0300

    Модул7 Видео2 Форматиране на формула 02 - за обем на вдлъбнат ъгъл

--- MP01/Calculations/Formula02.cs
+++ MP01/Calculations/Formula02.cs
@@ -4,6 +4,9 @@ namespace Calculations
 {
 	public class Formula02
 	{
+		Colors.ForCLI _cl=new Colors.ForCLI();
+
+		//Формула за вдлъбнат ъгъл
 		public Formula02 ()
 		{
 		}
@@ -24,13 +27,13 @@ namespace Calculations
 
 					if (runCalculate (param, out result)) 
 					{
-						Console.Write ("Обемът на вдлъбнатия ъгъл е: ");
-						Console.Write (result.ToString ("N2"));
-						Console.WriteLine (" m3 \n");
+						_cl.Default();	Console.Write ("Обемът на вдлъбнатия ъгъл е: ");
+						_cl.Result();	Console.Write (result.ToString ("N2"));
+						_cl.Default();	Console.WriteLine (" m3 \n");
 
 					} else{
 
-						Console.WriteLine ("Има грешно въведени параметрил\n С параметъра '-п' можете да видите синтаксиса на командата.\n");
+						_cl.Default();	Console.WriteLine ("Има грешно въведени параметрил\n С параметъра '-п' можете да видите синтаксиса на командата.\n");
 					}
 				}
 
@@ -60,17 +63,17 @@ namespace Calculations
 
 		private void help ()
 		{
-			Console.Write ("[въгъл]");
-			Console.WriteLine (" - вдлъбнат ъгъл");
+			_cl.Result ();	Console.Write ("[въгъл]");
+			_cl.Default ();	Console.WriteLine (" - вдлъбнат ъгъл");
 
-			Console.Write ("параметри:");
-			Console.WriteLine (" a b  и h ");
+			_cl.Command (); Console.Write ("параметри:");
+			_cl.Default ();	Console.WriteLine (" a b  и h \n");
 
-			Console.Write (" a и b");
-			Console.WriteLine (" - ширина и дължина");
+			_cl.Command ();	Console.Write (" a и b");
+			_cl.Default ();	Console.WriteLine (" - ширина и дължина");
 
-			Console.Write (" h ");
-			Console.WriteLine (" - височина");
+			_cl.Command ();	Console.Write (" h ");
+			_cl.Default ();	Console.WriteLine (" - височина");
 
 
 		}
