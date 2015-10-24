751417533c2ae2c8955a4837d1ab32ac17a7f780
danielasamardgieva <samardzhievad@gmail.com>
2015-10-24 12:51:54 +0300

    Упражнение върху трите формули за изкоп - работи

---
+++ MP001/Calculations/Formula02.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Calculations
+{
+	public class Formula02
+	{
+		Colors.ForCLI _cl=new Colors.ForCLI();
+
+		//Формула за вдлъбнат ъгъл
+		public Formula02 ()
+		{
+		}
+
+
+		public void calc (string _input)
+		{
+			try {
+				string[] param = _input.Split (' ');
+
+				if (param.Length > 1 && _input.ToLower().Contains("-п"))
+				{
+					help ();
+				}
+
+				if (param.Length == 4) {
+					double result = 0;
+
+					if (runCalculate (param, out result)) 
+					{
+						_cl.Default();	Console.Write ("Обемът на вдлъбнатия ъгъл е: ");
+						_cl.Result();	Console.Write (result.ToString ("N2"));
+						_cl.Default();	Console.WriteLine (" m3 \n");
+
+					} else{
+						_cl.Default();	Console.WriteLine ("Има грешно въведени параметрил\n С параметъра '-п' можете да видите синтаксиса на командата.\n");
+					}
+				}
+
+			} catch {
+
+			}
+		}
+
+		private bool runCalculate (String[] _param, out double _result)
+		{
+			try {
+				double a = 0;
+				Double.TryParse (_param [1], out a);
+				double b = 0;
+				Double.TryParse (_param [2], out b);
+				double h = 0;
+				Double.TryParse (_param [3], out h);
+
+				_result = a * b * h / 3;
+				return true;
+			} catch {
+			}
+			_result = 0;
+			return false;
+		}
+
+		private void help ()
+		{
+			_cl.Result ();	Console.Write ("[въгъл]");
+			_cl.Default ();	Console.WriteLine (" - вдлъбнат ъгъл");
+
+			_cl.Command (); Console.Write ("параметри:");
+			_cl.Default ();	Console.WriteLine (" a b  и h \n");
+
+			_cl.Command ();	Console.Write (" a и b");
+			_cl.Default ();	Console.WriteLine (" - ширина и дължина");
+
+			_cl.Command ();	Console.Write (" h ");
+			_cl.Default ();	Console.WriteLine (" - височина");
+
+		}
+
+	}
+
+}
+
--- MP001/UserInput/UserInput.cs
+++ MP001/UserInput/UserInput.cs
@@ -26,20 +26,14 @@ namespace UserInput
 				_c.Command(); _command=Console.ReadLine();
 
 			//Проверка за налични команди
-				if (_command.ToLower().Contains("яма"))  _calc.F01.calc(_command);
+				if (_command.ToLower().Contains("яма"))     _calc.F01.calc(_command);
 
-				if (_command.ToLower().Contains("команда2")) 
-				{
-					_c.Default(); Console.Write("Стартирана е ");
-					_c.Result();  Console.WriteLine("Команда2 \n");
-				}
-				if (_command.ToLower().Contains("команда3"))
+				if (_command.ToLower().Contains("въгъл"))   _calc.F02.calc(_command);
 
-				{
-					_c.Default(); Console.Write("Стартирана е ");
-					_c.Result();  Console.WriteLine("Команда3 \n");
-				}
 
+				if (_command.ToLower().Contains("иъгъл"))   _calc.F03.calc(_command);
+
+			
 
 			}while(_command != "изход");
 		}
